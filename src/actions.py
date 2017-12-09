d733932e51b49dd3802195ec60de14270049b1ba
Nicholas DiPinto <pinto18.nd@gmail.com>
2017-12-09 15:27:13 -0500

    Shutdown and reboot interact with a flask api now

--- src/actions.py
+++ src/actions.py
@@ -3,6 +3,7 @@
 import subprocess
 import time
 import os
+import requests
 import aiy.audio
 
 def say(words):
@@ -27,29 +28,37 @@ def action(phrase):
         #aiy.audio.say('Shutting down now.')
         say('Shutting down now.')
         time.sleep(10)
-        subprocess.call(
-            ["sudo",
-             "shutdown",
-             "-h",
-             "now"]
-        )
+        # subprocess.call(
+        #     ["sudo",
+        #      "shutdown",
+        #      "-h",
+        #      "now"]
+        # )
+        response = requests.get('http:localhost:5000/shutdown')
+        command = response.json()
+        os.system(command['command'])
     if 'boot' in phrase:
         #aiy.audio.say('Restarting now.')
         say('Restarting now.')
         time.sleep(10)
-        subprocess.call(
-            ["sudo",
-             "reboot"]
-        )
+        response = requests.get('http:localhost:5000/reboot')
+        command = response.json()
+        os.system(command['command'])
 
     if 'lamp' in phrase:
         if 'on' in phrase:
             say('Turning lamp on.')
-            subprocess.call(["curl",
-                             "http://192.168.104.239/LED=ON"]
-                            )
+            os.command(
+                        [
+                            "curl",
+                            "http://192.168.104.239/LED=ON"
+                        ]
+                      )
         elif 'off' in phrase:
             say('Turning lamp off.')
-            subprocess.call(["curl",
-                             "http://192.168.104.239/LED=OFF"]
-                            )
+            os.command(
+                        [
+                            "curl",
+                            "http://192.168.104.239/LED=OFF"
+                        ]
+                      )
