66e96e7cf23434e94e89052e213cf818af1189c1
Nicholas DiPinto <pinto18.nd@gmail.com>
2017-12-11 18:14:54 -0500

    Refactored some code v7

--- src/main.py
+++ src/main.py
@@ -65,7 +65,7 @@ def process_event(event):
         status_ui.status('ready')
 
     elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and
-    event.args['is_fatal':] 
+event.args['is_fatal':] 
         sys.exit(1)
 
 
