e8a9b8346aa05eefe5b642357590bb6d2565331f
Nicholas DiPinto <pinto18.nd@gmail.com>
2017-12-14 19:39:03 -0500

    Fixed syntax issues with python script for os commands container

--- src/apis/server_commands_api/raspi_os_commands.py
+++ src/apis/server_commands_api/raspi_os_commands.py
@@ -1,18 +1,17 @@
 #!flask/bin/python
 from flask import Flask, jsonify
-from flask.ext.api import status
 
 app = Flask(__name__)
 
-@app.route('/shutdown')
+@app.route('/shutdown', methods=['GET'])
 def shutdown():
     content = jsonify({'command' : 'sudo shutdown -h now'})
-    return content, status.HTTP_200_OK 
+    return content, 200 
 
-@app.route('/reboot')
+@app.route('/reboot', methods=['GET'])
 def reboot():
     content = jsonify({'command' : 'sudo reboot'}) 
-    return content, status.HTTP_200_OK 
+    return content, 200
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', debug=True)
